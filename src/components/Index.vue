e349b79df8d2fc88b12bce504cca91ceb0b2bc86
walkskyer <walkskyer@qq.com>
2020-04-20 14:12:36 +0800

    自定义组件

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      Index Page
-    </p>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <p>
+            Index Page
+            <ButtonCounter></ButtonCounter>
+        </p>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import ButtonCounter from "./widgets/ButtonCounter";
+
+    export default {
+        name: 'Index',
+        props: {
+            msg: String
+        },
+        components: {
+            ButtonCounter
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
